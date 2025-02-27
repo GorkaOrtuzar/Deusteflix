3a4bb4ee16ab4bcaaec8bc313bd1da0882033cd8
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2025-02-27 11:38:11 +0100

    Subiendo mi código C

--- menus.c
+++ menus.c
@@ -3,15 +3,16 @@
 char menuPrincipal(){
 	char opcion;
 
-	printf("1. Añadir nueva película\n");
-	printf("2. Ver todas las películas\n");
-	printf("3. Ordenar por año\n");
-	printf("4. Ordenar por título\n");
+	printf("1. Aï¿½adir nueva pelï¿½cula\n");
+	printf("2. Ver todas las pelï¿½culas\n");
+	printf("3. Ordenar por aï¿½o\n");
+	printf("4. Ordenar por tï¿½tulo\n");
 	printf("0. Salir\n");
-	printf("Elige una opción: ");
+	printf("Elige una opciï¿½n: ");
 	fflush(stdout);
 	fflush(stdin);
 	scanf("%c",&opcion);
 	return opcion;
 }
+
 
