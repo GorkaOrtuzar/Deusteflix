f41cf8acfb3965792e7df2de8e6ebba352aab4c1
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2025-05-29 19:27:07 +0200

    Logger.c y .h

---
+++ logger.h
@@ -0,0 +1,10 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#define FICH_LOG "log.txt"  // Define el nombre del archivo de log
+
+// Declaraciones de las funciones
+void escribirLog(char *mensaje);
+void insertarLog(char *mensaje);
+
+#endif
